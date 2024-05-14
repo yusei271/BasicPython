c2d7b9f9a46f0d2f0fc25f89145f8cc7d2d43552
yusei271 <y.fujimoto.24.4.1368@4nonome.com>
2024-05-14 15:35:46 +0900

    問３

--- euclid.py
+++ euclid.py
@@ -1,4 +1,31 @@
-a = input("a の値を入力: ")
-b = input("b の値を入力: ")
-
 # TODO
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+#問⑴
+num1_1 = 10
+num1_2 = 20
+
+result_1 = gcd(num1_1, num1_2)
+
+print("⑴の最大公約数:", result_1)
+
+#問⑵
+num2_1 = 14
+num2_2 = 91
+
+result_2 = gcd(num2_1, num2_2)
+
+print("⑵の最大公約数:", result_2)
+
+#問⑶
+num3_1 = 91
+num3_2 = 14
+
+result_3 = gcd(num3_1, num3_2)
+
+print("⑶の最大公約数:", result_3)
+
