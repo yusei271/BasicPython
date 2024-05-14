ec3f51dca13a3fdb76fe9fe6fe75a68c8c682a02
yusei271 <y.fujimoto.24.4.1368@4nonome.com>
2024-05-14 14:55:47 +0900

    問２

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -3,3 +3,16 @@ from math import sin
 # print(sin(0))
 # >>> 0
 # -----------
+from math import pi
+
+a = 0
+b = pi / 2 # pi = π
+n = 100
+
+h = (b - a) / n
+s = 0
+
+for k in range(1, n):
+    s = s + h / 2 * (sin(a + (k - 1) * h) + sin(a + k * h))
+
+print("求める積分値:", s)
